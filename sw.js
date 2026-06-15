de7fde624dd47c6ed3c71c334723ac2b8e4414f6
jeremylejoly <jeremylejoly@gmail.com>
2026-06-15 15:42:46 +0200

    feat(dashboard): remplacer l'émoji par le nouveau logo dans la bannière d'accueil

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for TBI Enseignant — sw.js
-const CACHE_NAME = 'tbi-enseignant-cache-v52';
+const CACHE_NAME = 'tbi-enseignant-cache-v53';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
