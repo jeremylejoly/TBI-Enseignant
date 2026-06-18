503ca5d375737905fe999e62d05e0d364601a8a5
jeremylejoly <jeremylejoly@gmail.com>
2026-06-18 09:25:21 +0200

    feat(belgium-map): remove Quiz LCMA sidebar block

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for TBI Enseignant — sw.js
-const CACHE_NAME = 'tbi-enseignant-cache-v68';
+const CACHE_NAME = 'tbi-enseignant-cache-v69';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
