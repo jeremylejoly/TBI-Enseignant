39fd560ad78f19449dfe96f6285a9db3aa4844f4
jeremylejoly <jeremylejoly@gmail.com>
2026-06-19 13:03:37 +0200

    fix(horaire): reset schedule zoom in print stylesheet

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for TBI Enseignant — sw.js
-const CACHE_NAME = 'tbi-enseignant-cache-v74';
+const CACHE_NAME = 'tbi-enseignant-cache-v75';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
