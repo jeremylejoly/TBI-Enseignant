1282345a60035f9bcedd91ea0eb948724348983d
jeremylejoly <jeremylejoly@gmail.com>
2026-07-05 22:45:31 +0200

    modifs abaque numération

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for TBI Enseignant — sw.js
-const CACHE_NAME = 'tbi-enseignant-cache-v85';
+const CACHE_NAME = 'tbi-enseignant-cache-v86';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
