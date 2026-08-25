f455956be13d33bd9118b48e57c7a560d987af27
jeremylejoly <jeremylejoly@gmail.com>
2026-08-25 13:14:47 +0200

    fix(semainier): persistance semaine active, separation etanche modele/semaines, et conservation donnees lors de l edition

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for TBI Enseignant — sw.js
-const CACHE_NAME = 'tbi-enseignant-cache-v99';
+const CACHE_NAME = 'tbi-enseignant-cache-v100';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
